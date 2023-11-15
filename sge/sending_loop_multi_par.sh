4dcec81c3cd5daf99a3a6439142472667683ef30
swom <cicciacco@gmail.com>
2023-11-16 00:40:23 +0100

    adding scripts for launching and debugging

---
+++ sge/sending_loop_multi_par.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --time=00:10:00
+#SBATCH --mem=500MB
+#SBATCH --job-name=bt_sending_loop
+#SBATCH --output=bt_sending_loop%j.log
+
+
+
+# Declare arrays 
+DEV=("0.01" "0.001")
+META=("1.0" "0.1") 
+MUT_R=("0.1" "0.01")
+RUN=($(seq 0 1 30))
+
+# Nested loops to iterate over permutations
+for dev in "${DEV[@]}"
+do
+  for meta in "${META[@]}"
+  do
+    for mut in "${MUT_R[@]}"
+    do 
+      for run in "${RUN[@]}"
+      do
+        echo "dev: $dev, meta: $meta, start_mut_rate: $mut, run: $run"
+        sbatch sending_trillions.sh $dev $meta $run $mut
+      done
+    done
+  done
+done
---
+++ sge/sending_trillions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=12:10:00
+#SBATCH --mem=500MB
+#SBATCH --job-name=psge_job
+#SBATCH --output=logs/psge_job%j.log
+
+
+export PATH=$HOME/.local/bin:$PATH
+module load Python
+pip install numpy
+pip install PyYAML
+pip install tqdm
+
+python -m examples.symreg --experiment_name /scratch/p288427/megalomania/psge --run $5 --seed $5 --parameters parameters/standard.yml --grammar grammars/regression_pagie.pybnf --gauss_sd $1 --prob_mutation_probs $2 --delay $3 --remap $4 --prob_mutation $6
